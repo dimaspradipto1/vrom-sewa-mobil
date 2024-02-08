005a046658dcd1e001df9725919e248cfe7d1a8f
dimas <dimaspradipto@gmail.com>
2024-02-08 22:21:26 +0700

    create brand request and brand controller

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,6 +16,11 @@
             {{ __('Dashboard') }}
           </x-nav-link>
         </div>
+        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+          <x-nav-link href="{{ route('admin.brands.index') }}" :active="request()->routeIs('admin.brands.index')">
+            {{ __('Brands') }}
+          </x-nav-link>
+        </div>
       </div>
 
       <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -150,6 +155,9 @@
     <div class="pt-2 pb-3 space-y-1">
       <x-responsive-nav-link href="{{ route('admin.dashboard') }}" :active="request()->routeIs('admin.dashboard')">
         {{ __('Dashboard') }}
+      </x-responsive-nav-link>
+      <x-responsive-nav-link href="{{ route('admin.brands.index') }}" :active="request()->routeIs('admin.brands.index')">
+        {{ __('Brands') }}
       </x-responsive-nav-link>
     </div>
 
