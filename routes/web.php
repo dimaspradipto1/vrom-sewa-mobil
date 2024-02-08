4969a8c9a4ce23408cd84167e1e0d2bf84c4de73
dimas <dimaspradipto@gmail.com>
2024-02-08 21:07:24 +0700

    update navigation menu

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,14 @@ use App\Http\Controllers\Admin\BrandController as AdminBrandController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('front.index');
 
-Route::middleware([
+Route::prefix('admin')->name('admin.')->middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
+    'admin'
 ])->group(function () {
-    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+    Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     Route::resource('brands', AdminBrandController::class);
 });
